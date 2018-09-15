42202fbd5933cf3a6ab9da13efa36458aa6b30bf
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-09-14 23:11:27 -0400

    render method with react lifecycle

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
-import reducers from './reducers';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+      </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
